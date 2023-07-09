76a8441490e29b0d56aafee1b0edabbcd72c08a5
Sergei-Bug <122543402+Sergei-Bug@users.noreply.github.com>
2023-07-09 17:24:45 +0300

    completed

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,49 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const wrapperGallary = document.querySelector(".gallery");
+
+// create mark up
+const galleryCreateMarkUp = galleryItems
+  .map(({ preview, original, description }) => {
+    return `<li class="gallery__item">
+        <a class="gallery__link" href="large-image.img">
+            <img
+                class="gallery__image"
+                src="${preview}"
+                data-source="${original}"
+                alt="${description}"
+            />
+        </a>
+    </li>`;
+  })
+  .join("");
+
+//   add mark up
+
+wrapperGallary.insertAdjacentHTML("beforeend", galleryCreateMarkUp);
+
+wrapperGallary.addEventListener("click", onElGalleryClick);
+
+function onElGalleryClick(ev) {
+  ev.preventDefault();
+  if (ev.target.nodeName !== "IMG") {
+    return;
+  }
+
+  const instance = basicLightbox.create(`
+    <img src="${ev.target.dataset.source}" wight="800" height="600">`);
+  instance.show();
+
+  document.addEventListener(
+    "keydown",
+    (ev) => {
+      if (ev.code === "Escape") {
+        instance.close();
+      }
+    },
+    { once: true }
+  );
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,40 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const wrapperGallary = document.querySelector(".gallery");
+
+// create mark up
+const galleryCreateMarkUp = galleryItems
+  .map(({ preview, original, description }) => {
+    return `<li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img
+                class="gallery__image"
+                src="${preview}"
+                alt="${description}"
+            />
+        </a>
+    </li>`;
+  })
+  .join("");
+
+//   add mark up
+
+wrapperGallary.insertAdjacentHTML("beforeend", galleryCreateMarkUp);
+
+wrapperGallary.addEventListener("click", onGalleryItemClick);
+
+var lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
+
+function onGalleryItemClick(ev) {
+  ev.preventDefault();
+
+  if (ev.target.nodeName !== "IMG") {
+    return;
+  }
+}
